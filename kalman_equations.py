754aaae68796e1cf2ba65bb7b54f45fd151155fd
Elisa Specchia <elisa.specchia@gmail.com>
2020-12-30 23:33:29 +0100

    kalman equations and tests

--- kalman_equations.py
+++ kalman_equations.py
@@ -1,5 +1,27 @@
-def state_extrapolation_equation(x, v, delta_time):
-    next_x = x + v * delta_time
-    next_v = v
-    return next_x, next_v
+def state_extrapolation(current_estimate_x, current_estimate_v, delta_time):
+    next_estimate_x = current_estimate_x + current_estimate_v * delta_time
+    next_estimate_v = current_estimate_v
+    return next_estimate_x, next_estimate_v
 
+
+def state_update(previous_estimate_x, previous_estimate_v, measure, delta_time, kg):
+    current_estimate_x = previous_estimate_x + kg * (measure - previous_estimate_x)
+    current_estimate_v = previous_estimate_v + kg * ((measure - previous_estimate_x) / delta_time)
+    return current_estimate_x, current_estimate_v
+
+
+def kalman_gain(previous_estimate_uncertainty_x, measure_uncertainty_x):
+    kg = previous_estimate_uncertainty_x / (previous_estimate_uncertainty_x + measure_uncertainty_x)
+    return kg
+
+
+def estimate_uncertainty_extrapolation(current_estimate_uncertainty_x, current_estimate_uncertainty_v, delta_time):
+    next_estimate_uncertainty_x = current_estimate_uncertainty_x + (delta_time ** 2) * current_estimate_uncertainty_v
+    next_estimate_uncertainty_v = current_estimate_uncertainty_v
+    return next_estimate_uncertainty_x, next_estimate_uncertainty_v
+
+
+def estimate_uncertainty_update(previous_estimate_uncertainty_x, previous_estimate_uncertainty_v, kg):
+    current_estimate_uncertainty_x = (1 - kg) * previous_estimate_uncertainty_x
+    current_estimate_uncertainty_v = (1 - kg) * previous_estimate_uncertainty_v
+    return current_estimate_uncertainty_x, current_estimate_uncertainty_v
--- tests/kalman_equations_test.py
+++ tests/kalman_equations_test.py
@@ -1,15 +1,100 @@
-from kalman_equations import state_extrapolation_equation
+import random
+import pytest
+import kalman_equations as kalman
 
 
-def test_state_extrapolation_equation():
-    x = 30000
-    v = 40
+def test_state_extrapolation():
+    current_estimate_x = 30000
+    current_estimate_v = 40
     delta_time = 5
 
-    next_x, next_v = state_extrapolation_equation(x, v, delta_time)
+    next_estimate_x, next_estimate_v = kalman.state_extrapolation(
+        current_estimate_x, current_estimate_v, delta_time)
 
     expected_x = 30200
     expected_v = 40
 
-    assert next_x == expected_x
-    assert next_v == expected_v
+    assert next_estimate_x == expected_x
+    assert next_estimate_v == expected_v
+
+
+def build_test_state_update_by_kg(kg, expected_x, expected_v):
+    previous_estimate_x = 30000
+    previous_estimate_v = 40
+    measure = 30095
+    delta_time = 5
+
+    current_estimate_x, current_estimate_v = kalman.state_update(
+        previous_estimate_x, previous_estimate_v, measure,
+        delta_time, kg)
+
+    assert current_estimate_x == expected_x
+    assert current_estimate_v == expected_v
+
+
+def test_state_update():
+    build_test_state_update_by_kg(0.5, 30047.5, 49.5)
+
+
+def test_state_update_kg_0():
+    build_test_state_update_by_kg(0, 30000, 40)
+
+
+def test_state_update_kg_1():
+    build_test_state_update_by_kg(1, 30095, 59)
+
+
+def test_kalman_gain_range():
+    for i in range(0, 10000):
+        previous_estimate_uncertainty_x = random.uniform(0.1, 10000)
+        measure_uncertainty_x = random.uniform(0.1, 10000)
+        kg = kalman.kalman_gain(previous_estimate_uncertainty_x, measure_uncertainty_x)
+
+        assert kg >= 0
+        assert kg <= 1
+
+
+def test_kalman_gain_zero_division():
+    previous_estimate_uncertainty_x = 0
+    measure_uncertainty_x = 0
+
+    with pytest.raises(ZeroDivisionError):
+        kalman.kalman_gain(previous_estimate_uncertainty_x, measure_uncertainty_x)
+
+
+def test_estimate_uncertainty_extrapolation():
+    current_estimate_uncertainty_x = 10000
+    current_estimate_uncertainty_v = 100
+    delta_time = 5
+
+    next_estimate_uncertainty_x, next_estimate_uncertainty_v = kalman.estimate_uncertainty_extrapolation(
+        current_estimate_uncertainty_x, current_estimate_uncertainty_v, delta_time)
+
+    expected_x = 12500
+    expected_v = 100
+
+    assert next_estimate_uncertainty_x == expected_x
+    assert next_estimate_uncertainty_v == expected_v
+
+
+def build_test_estimate_uncertainty_update_by_kg(kg, expected_x, expected_v):
+    previous_estimate_uncertainty_x = 10000
+    previous_estimate_uncertainty_v = 100
+
+    current_estimate_uncertainty_x, current_estimate_uncertainty_v = kalman.estimate_uncertainty_update(
+        previous_estimate_uncertainty_x, previous_estimate_uncertainty_v, kg)
+
+    assert current_estimate_uncertainty_x == expected_x
+    assert current_estimate_uncertainty_v == expected_v
+
+
+def test_estimate_uncertainty_update():
+    build_test_estimate_uncertainty_update_by_kg(0.5, 5000, 50)
+
+
+def test_estimate_uncertainty_update_kg_0():
+    build_test_estimate_uncertainty_update_by_kg(0, 10000, 100)
+
+
+def test_estimate_uncertainty_update_kg_1():
+    build_test_estimate_uncertainty_update_by_kg(1, 0, 0)
